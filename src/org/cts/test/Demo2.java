80d3fd2dec00830416219fe2599ad07d8411d117
unknown <sofiselva34@gmail.com>
2019-09-21 12:35:37 +0530

    Coomitted my code

---
+++ src/org/cts/test/Demo2.java
@@ -0,0 +1,24 @@
+package org.cts.test;
+
+public class Demo2 {
+	
+	public static void main(String[] args) {
+		int age = 10;
+		  
+		  if( age<18)
+		  
+		  {
+			  
+			  System.out.println("You are not eligible to vote");
+		  }
+		  
+		  
+		  
+		  
+		  }
+		  
+		
+		
+	}
+
+
